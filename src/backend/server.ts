d3f71053cef19bcbebbefa7fcb2a4e5f234e4322
AnshKhandelwal19 <khandelwalansh8@gmail.com>
2025-10-30 19:33:48 -0700

    added lobby route

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -5,10 +5,11 @@ import createHttpError from "http-errors";
 
 import rootRoutes from "./routes/root";
 import { testRouter } from "./routes/test";
+import { lobbyRoutes } from "./routes/lobby";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(morgan("dev"));
 app.use(express.static(path.join("dist", "public")));
@@ -17,6 +18,7 @@ app.set("view engine", "ejs");
 
 app.use("/", rootRoutes);
 app.use("/test", testRouter);
+app.use("/lobby", lobbyRoutes)
 
 app.use((_request, _response, next) => {
   next(createHttpError(404));
