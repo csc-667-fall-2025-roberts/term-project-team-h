d851e87ac5854aa6d5635de1d3bae43d392c9304
AnshKhandelwal19 <khandelwalansh8@gmail.com>
2025-11-29 19:37:07 -0800

    root, login, and register pages complete

---
+++ src/backend/routes/login.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (_request, response) => {
+  response.render("login", {});
+});
+
+router.post("/", (request, response) => {
+    console.log(request.body.username);
+    console.log(request.body.password);
+
+    response.render("login", {});
+});
+
+export { router as loginRoutes };
--- src/backend/server.ts
+++ src/backend/server.ts
@@ -6,19 +6,25 @@ import createHttpError from "http-errors";
 import rootRoutes from "./routes/root";
 import { testRouter } from "./routes/test";
 import { lobbyRoutes } from "./routes/lobby";
+import { loginRoutes } from "./routes/login";
+import { registerRoutes } from "./routes/register";
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 app.use(morgan("dev"));
-app.use(express.static(path.join("dist", "public")));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use("/", rootRoutes);
 app.use("/test", testRouter);
-app.use("/lobby", lobbyRoutes)
+app.use("/lobby", lobbyRoutes);
+app.use("/login", loginRoutes);
+app.use("/register", registerRoutes);
 
 app.use((_request, _response, next) => {
   next(createHttpError(404));
