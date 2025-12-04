5cb017a6006d73b141aa722001fcd9f1f823cdc9
JoseGPascua <146772240+JoseGPascua@users.noreply.github.com>
2025-12-03 23:07:15 -0800

    adding migration for hashing passwords to 60 char, added bcrypt to later implement

--- migrations/1764744457315_create-users.ts
+++ migrations/1764744457315_create-users.ts
@@ -3,10 +3,25 @@ import { MigrationBuilder } from "node-pg-migrate";
 export async function up(pgm: MigrationBuilder): Promise<void> {
   pgm.createTable("users", {
     id: "id" as any,
-    username: { type: "varchar(50)", notNull: true, unique: true },
-    email: { type: "varchar(100)", notNull: true, unique: true },
-    password: { type: "varchar(255)", notNull: true },
-    created_at: { type: "timestamp", default: pgm.func("current_timestamp") } as any
+    username: {
+      type: "varchar(50)",
+      notNull: true,
+      unique: true,
+    },
+    email: {
+      type: "varchar(100)",
+      notNull: true,
+      unique: true,
+    },
+    password: {
+      type: "varchar(255)",
+      notNull: true,
+    },
+    created_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    } as any,
   });
 }
 
